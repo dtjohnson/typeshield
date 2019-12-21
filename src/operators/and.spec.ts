6843b40c7764b14bfcab736e91c2ed5f8dd6b92c
Dave Johnson <david.johnson@modernatx.com>
2019-12-21 08:21:53 -0500

    Added operator tests

---
+++ src/operators/and.spec.ts
@@ -0,0 +1,22 @@
+import { and } from './and';
+import { assertValue } from '../assert';
+import { hasDefinition } from '../guards/has-definition';
+import { isBoolean } from '../guards/is-boolean';
+
+const isFoo = hasDefinition({ foo: isBoolean }, 'Foo');
+const isBar = hasDefinition({ bar: isBoolean }, 'Bar');
+
+describe(and.name, () => {
+    it('should create a combination guard', () => {
+        const isFooAndBar = and(isFoo, isBar);
+        expect(isFooAndBar({ foo: true, bar: true })).toBe(true);
+        expect(isFooAndBar({ foo: true })).toBe(false);
+        expect(isFooAndBar({ bar: true })).toBe(false);
+        expect(isFooAndBar({})).toBe(false);
+        expect(isFooAndBar(undefined)).toBe(false);
+    });
+
+    it('should throw with a clear message', () => {
+        expect(() => assertValue(and(isFoo, isBar), undefined)).toThrow('Expected value to implement \'Foo\' and to implement \'Bar\' but received: undefined');
+    });
+});
---
+++ src/operators/or.spec.ts
@@ -0,0 +1,18 @@
+import { assertValue } from '../assert';
+import { isNumber } from '../guards/is-number';
+import { isString } from '../guards/is-string';
+import { or } from './or';
+
+describe(or.name, () => {
+    it('should create a combination guard', () => {
+        const isStringOrNumber = or(isString, isNumber);
+        expect(isStringOrNumber(5.7)).toBe(true);
+        expect(isStringOrNumber('foo')).toBe(true);
+        expect(isStringOrNumber(true)).toBe(false);
+        expect(isStringOrNumber(undefined)).toBe(false);
+    });
+
+    it('should throw with a clear message', () => {
+        expect(() => assertValue(or(isString, isNumber), undefined)).toThrow('Expected value to be a string or to be a number but received: undefined');
+    });
+});
